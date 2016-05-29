011136f9393517be875fe8c42c7d39310d3cf988
MayanaPereira <mayanaw@gmail.com>
2016-05-28 19:22:52 -0700

    typo corrected

--- MulticlassAUC.R
+++ MulticlassAUC.R
@@ -7,7 +7,7 @@ library(ROCR)
 
 ##multiclass AUC for 3-class classification
 Multiclass_AUC <- function(probs, true){
-  #Mapping True Vector: positive class -> mapped to 1; necative class -> mapped to 0
+  #Mapping True Vector: positive class -> mapped to 1; negative class -> mapped to 0
   true1<- true
   true1<- as.numeric(true1)
   true1[true1==1]<-1
@@ -18,7 +18,7 @@ Multiclass_AUC <- function(probs, true){
   #use auc function from pROC library
   aucp1 <- auc(true1, probs.class.1)
 
-  #Mapping True Vector: positive class -> mapped to 1; necative class -> mapped to 0
+  #Mapping True Vector: positive class -> mapped to 1; negative class -> mapped to 0
   true2<- true
   true2<- as.numeric(true2)
   true2[true2==1]<-0
@@ -28,7 +28,7 @@ Multiclass_AUC <- function(probs, true){
   probs.class.2 <-as.numeric(subset(probs,select = c(2)))
   aucp2 <- auc(true2, probs.class.2)
 
-  #Mapping True Vector: positive class -> mapped to 1; necative class -> mapped to 0
+  #Mapping True Vector: positive class -> mapped to 1; negative class -> mapped to 0
   true3<- true
   true3<- as.numeric(true3)
   true3[true3==1]<-0
